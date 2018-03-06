cebb0c9d4f55d4f77f4bc95107e5272f28d2c2cd
agaszab <gatito@poczta.pl>
2018-03-06 21:11:12 +0100

    poprawienie przekazywania obiektów

--- src/main/java/pl/javascript/Main.java
+++ src/main/java/pl/javascript/Main.java
@@ -8,10 +8,6 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         SmartHome smartHome = context.getBean(SmartHome.class);
-        StacjaPogodowa stacjaPogodowa = context.getBean(StacjaPogodowa.class);
-        SterownikRolet sterownikRolet = context.getBean(SterownikRolet.class);
-        SterownikGlosnika sterownikGlosnika = context.getBean(SterownikGlosnika.class);
-        SterownikPieca sterownikPieca =  context.getBean(SterownikPieca.class);
         smartHome.home();
     }
 }
